873cfc05e57b603dde16fd8d592fbcdd1dfef900
Andrew Morrison <morrison.andrew422@gmail.com>
2024-01-22 22:13:39 -0500

    finish timer functionality

--- src/Components/Timer/Timer.jsx
+++ src/Components/Timer/Timer.jsx
@@ -1,87 +1,104 @@
-import { useState, useEffect } from 'react';
-import { useSelector } from 'react-redux';
-import styles from './Timer.module.css';
+import React, { useState, useEffect } from 'react'
+import styles from './Timer.module.css'
+import { useSelector } from 'react-redux'
 
 const Timer = ({ steak }) => {
-  const [timeRemaining, setTimeRemaining] = useState(0);
-  const [waitTimeRemaining, setWaitTimeRemaining] = useState(0);
-  const [waitTimerActive, setWaitTimerActive] = useState(true);
+  const [preFlipTimeRemaining, setPreFlipTimeRemaining] = useState(0)
+  const [postFlipTimeRemaining, setPostFlipTimeRemaining] = useState(0)
+  const [waitTimeRemaining, setWaitTimeRemaining] = useState(0)
 
-  const steakData = useSelector((state) => state.steak);
+  const steakData = useSelector((state) => state.steak)
 
   const findLongestCookTime = () => {
-    let longestCookTime = 0;
+    let longestCookTime = 0
 
     steakData.forEach((s) => {
       if (s.totalTime > longestCookTime) {
-        longestCookTime = s.totalTime;
+        longestCookTime = s.totalTime
       }
-    });
+    })
 
     return longestCookTime;
   };
 
-  const findCookTime = () => {
-    const selectedSteak = steakData.find((s) => s.id === steak.id);
-    return selectedSteak ? selectedSteak.totalTime : 0;
-  };
-
+  // useEffect for Longest Cook Time Calculation:
   useEffect(() => {
-    const selectedCookTime = findCookTime();
-    const longestCookTime = findLongestCookTime();
-    const totalSeconds = selectedCookTime * 60;
+    const longestCookTime = findLongestCookTime()
 
-    setTimeRemaining(totalSeconds);
-
-    if (selectedCookTime < longestCookTime) {
-      const waitTimeSeconds = (longestCookTime - selectedCookTime) * 60;
-      setWaitTimeRemaining(waitTimeSeconds);
-    } else {
-      setWaitTimerActive(false);
+    // Set wait time if the steak doesn't have the longest cook time
+    if (steak.totalTime < longestCookTime) {
+      const waitTimeSeconds = (longestCookTime - steak.totalTime) * 60
+      setWaitTimeRemaining(waitTimeSeconds)
     }
+  }, [steak, steakData])
 
+  // useEffect for Wait Timer Countdown:
+  useEffect(() => {
     // Update the countdown every second for wait timer
     const waitTimerInterval = setInterval(() => {
-      setWaitTimeRemaining((prevTime) => (prevTime > 0 ? prevTime - 1 : 0));
-    }, 1000);
+      setWaitTimeRemaining((prevTime) => (prevTime > 0 ? prevTime - 1 : 0))
+    }, 1000)
 
     // Clean up wait timer interval on component unmount or when wait timer is up
-    return () => {
-      clearInterval(waitTimerInterval);
-      setWaitTimerActive(false);
-    };
-  }, [steak, steakData]);
+    return () => clearInterval(waitTimerInterval)
+  }, [])
 
+  // useEffect for Pre-Flip Timer Initialization:
   useEffect(() => {
-    // Update the countdown every second for main timer when wait timer is finished
-    if (waitTimeRemaining === 0 && !waitTimerActive) {
-      const mainTimerInterval = setInterval(() => {
-        setTimeRemaining((prevTime) => (prevTime > 0 ? prevTime - 1 : 0));
-      }, 1000);
-
-      // Clean up main timer interval on component unmount
-      return () => clearInterval(mainTimerInterval);
+    // If there is no wait timer, start the pre-flip timer immediately
+    if (waitTimeRemaining === 0) {
+      setPreFlipTimeRemaining(steak.cookTimes[0] * 60)
     }
-  }, [waitTimeRemaining, waitTimerActive]);
+  }, [waitTimeRemaining, steak])
 
-  // Format seconds into minutes:seconds
+  // useEffect for Post-Flip Timer Initialization:
+  useEffect(() => {
+    // Start the post-flip timer when pre-flip timer is finished
+    if (preFlipTimeRemaining === 0) {
+      setPostFlipTimeRemaining(steak.cookTimes[1] * 60)
+      const postFlipTimerInterval = setInterval(() => {
+        setPostFlipTimeRemaining((prevTime) => (prevTime > 0 ? prevTime - 1 : 0))
+      }, 1000)
+
+      // Clean up post-flip timer interval on component unmount
+      return () => clearInterval(postFlipTimerInterval)
+    }
+  }, [preFlipTimeRemaining, steak])
+
+  // useEffect for Pre-Flip Timer Countdown:
+  useEffect(() => {
+    // Start the pre-flip timer when wait timer is finished
+    if (waitTimeRemaining === 0) {
+      const preFlipTimerInterval = setInterval(() => {
+        setPreFlipTimeRemaining((prevTime) => (prevTime > 0 ? prevTime - 1 : 0))
+      }, 1000)
+
+      // Clean up pre-flip timer interval on component unmount
+      return () => clearInterval(preFlipTimerInterval)
+    }
+  }, [waitTimeRemaining])
+
+
   const formatTime = (seconds) => {
-    const minutes = Math.floor(seconds / 60);
-    const remainingSeconds = seconds % 60;
-    return `${String(minutes).padStart(2, '0')}:${String(remainingSeconds).padStart(2, '0')}`;
-  };
+    const minutes = Math.floor(seconds / 60)
+    const remainingSeconds = seconds % 60
+    return `${String(minutes).padStart(2, '0')}:${String(remainingSeconds).padStart(2, '0')}`
+  }
 
+  // render
   return (
     <div className={styles.timer}>
-      {waitTimeRemaining > 0 ? (
+      {waitTimeRemaining > 0 && (
         <div>
           Wait Timer: {formatTime(waitTimeRemaining)}
           <br />
         </div>
-      ) : null}
-      Main Timer: {formatTime(timeRemaining)}
+      )}
+      Pre-Flip Timer: {formatTime(preFlipTimeRemaining)}
+      <br />
+      Post-Flip Timer: {formatTime(postFlipTimeRemaining)}
     </div>
   );
 };
 
-export default Timer;
+export default Timer
